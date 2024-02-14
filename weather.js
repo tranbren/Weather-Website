7c65b50b6813764d28bead2d631e112ebfaa3f9a
Brendon <156395453+tranbren@users.noreply.github.com>
2024-02-13 22:02:37 -0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const apiKey = "URLKEY";
+const apiKey = "af68708a159a8a2a3210c8030b11de3b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=";
 
 const searchBox = document.querySelector(".search input");
